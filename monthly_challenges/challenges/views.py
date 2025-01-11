1bb684523ac311e4958e9bba8c205547d60ff392
arya03 <aryachauhan03@gmail.com>
2025-01-11 14:16:40 +0530

    Run view dynamically based on month present in url

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,7 +1,16 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse('Eat no meat for the entire month!')
+def monthly_challenge(request, month):
+    challenge_text = None
+    if(month == 'january'):
+        challenge_text = 'Eat no meat for the entire month!'
+    elif(month == 'february'):
+        challenge_text = 'Walk for atleat 20 minutes every day!'
+    elif(month == 'march'):
+        challenge_text = 'Learn Django for at least 20 minutes every day!'
+    else:
+        return HttpResponseNotFound('This month is not supported!')
+    return HttpResponse(challenge_text)
