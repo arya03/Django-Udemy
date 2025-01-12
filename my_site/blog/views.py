62ec0f695bc49fe35873066da97363efe5680402
arya03 <aryachauhan03@gmail.com>
2025-01-12 20:39:21 +0530

    Update Author name and dates of blogs

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -6,8 +6,8 @@ all_posts = [
     {
         "slug": "hike-in-the-mountains",
         "image": "mountains.jpg",
-        "author": "Maximilian",
-        "date": date(2021, 7, 21),
+        "author": "Arya",
+        "date": date(2025, 1, 12),
         "title": "Mountain Hiking",
         "excerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoying the view!",
         "content": """
@@ -27,8 +27,8 @@ all_posts = [
     {
         "slug": "programming-is-fun",
         "image": "coding.jpg",
-        "author": "Maximilian",
-        "date": date(2022, 3, 10),
+        "author": "Arya",
+        "date": date(2025, 1, 11),
         "title": "Programming Is Great!",
         "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
         "content": """
@@ -48,8 +48,8 @@ all_posts = [
     {
         "slug": "into-the-woods",
         "image": "woods.jpg",
-        "author": "Maximilian",
-        "date": date(2020, 8, 5),
+        "author": "Arya",
+        "date": date(2025, 1, 10),
         "title": "Nature At Its Best",
         "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
         "content": """
