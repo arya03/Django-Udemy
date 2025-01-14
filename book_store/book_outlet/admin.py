47f19b40cfeacff1e73bf99e9102b9d243aa1689
arya03 <aryachauhan03@gmail.com>
2025-01-14 23:18:08 +0530

    handle admin panel for book outlet.

--- book_store/book_outlet/admin.py
+++ book_store/book_outlet/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import Book
+
 # Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+  # readonly_fields = ('slug',)  # Prevent slug from being edited
+  prepopulated_fields = {'slug': ('title',)}  # Auto-generate slug from title
+  list_filter = ('author', 'rating',)
+  list_display = ('title', 'author', 'rating', 'is_bestSelling')
+
+admin.site.register(Book, BookAdmin)
