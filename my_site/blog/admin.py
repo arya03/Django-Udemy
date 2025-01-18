cfd91476a0ca0ddf5c91e800427891f439925f27
arya03 <aryachauhan03@gmail.com>
2025-01-18 23:02:01 +0530

    Fetch and render blogs from database in my_site project.

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Tag, Author, Post
 
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+  prepopulated_fields = {'slug': ('title',)}
+  list_filter = ('author', 'date', 'tags',)
+  list_display = ('title', 'author', 'date')
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Author)
+admin.site.register(Tag)
