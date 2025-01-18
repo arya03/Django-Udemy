940b9e983e48ed0b387411fc23a6e9255a28e48b
arya03 <aryachauhan03@gmail.com>
2025-01-18 16:32:09 +0530

    Add 1-1, 1-many and many-many relationships.

--- book_store/book_outlet/admin.py
+++ book_store/book_outlet/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Address, Author, Book, Country
 
 # Register your models here.
 
@@ -11,3 +11,6 @@ class BookAdmin(admin.ModelAdmin):
   list_display = ('title', 'author', 'rating', 'is_bestSelling')
 
 admin.site.register(Book, BookAdmin)
+admin.site.register(Author)
+admin.site.register(Address)
+admin.site.register(Country)
