6fd553f2996cc17cf486f58db1a20f29efe2cf37
arya03 <aryachauhan03@gmail.com>
2025-01-25 22:26:54 +0530

    Implemented Forms Class logic

---
+++ feedback/reviews/views.py
@@ -0,0 +1,43 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.views import View
+from .forms import ReviewForm
+from .models import Review
+
+# Create your views here.
+
+# Class-based views
+
+class ReviewView(View):
+  def get(self, request):
+    form = ReviewForm()
+    return render(request, "reviews/review.html", {
+      "form": form
+  })
+    
+  def post(self, request):
+    form = ReviewForm(request.POST)
+    if form.is_valid():
+      form.save()
+      return HttpResponseRedirect("/thank-you")
+    
+    return render(request, "reviews/review.html", {
+      "form": form
+  })
+
+# def review(request):
+#   if request.method == "POST":
+#     form = ReviewForm(request.POST)
+#     if form.is_valid():
+#       # entered_username = form.cleaned_data["user_name"]
+#       form.save()
+#       return HttpResponseRedirect("/thank-you")
+  
+#   else:
+#     form = ReviewForm()
+#   return render(request, "reviews/review.html", {
+#       "form": form
+#   })
+
+def thank_you(request):
+  return render(request, "reviews/thank_you.html")
